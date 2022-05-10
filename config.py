5b1b68431c970702a67f9a56735e41aa397b63ea
Moritz Eich <hey@moritz.dev>
2022-05-10 16:12:51 +0200

    fix: pipeline

--- config.py
+++ config.py
@@ -3,15 +3,17 @@ from pydantic import BaseSettings, Field
 
 
 class EnvConfig(BaseSettings):
-    OPENAI_API_KEY: str = Field(env="OPENAI_API_KEY")
-    MONGO_DB_CONNECTION: str = Field(env="MONGO_DB_CONNECTION")
+    OPENAI_API_KEY: str = Field(None, env="OPENAI_API_KEY")
+    MONGO_DB_CONNECTION: str = Field(
+        "mongodb://127.0.0.1:27017", env="MONGO_DB_CONNECTION"
+    )
     USER_RATE_LIMIT_PER_MINUTE: str = Field(5, env="USER_RATE_LIMIT_PER_MINUTE")
     DECK_SERVICE_HOST_NAME: str = Field(
         "deck-management-service", env="DECK_MANAGEMENT_SERVICE_HOST_NAME"
     )
     DATABASE: str = Field("spacey", env="DATABASE")
     ENV: str = Field("development", env="ENV")
-    MODEL_CONFIG_ID: str = Field(env="MODEL_CONFIG_ID")
+    MODEL_CONFIG_ID: str = Field(None, env="MODEL_CONFIG_ID")
 
 
 load_dotenv(find_dotenv())
