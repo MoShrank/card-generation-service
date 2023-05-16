a37eb8f4888091efc5d0a1f503cc7a11acee1746
Moritz Eich <hey@moritz.dev>
2023-05-16 23:16:10 +0200

    change: model config & add summarizer config

--- models/ModelConfig.py
+++ models/ModelConfig.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Literal
 
 from pydantic import BaseModel, Field
 
@@ -15,15 +15,34 @@ class ModelParameters(BaseModel):
     temperature: int
     model: str
     max_tokens: int
+    presence_penalty: int = Field(default=0)
+    frequency_penalty: int = Field(default=0)
     top_p: int
     n: int
     stop_sequence: List[str]
 
 
+class Message(BaseModel):
+    role: Literal["user", "system", "assistant"]
+    content: str
+
+
+Messages = List[Message]
+
+
 class ModelConfig(BaseModel):
     id: PyObjectID = Field(default_factory=PyObjectID, alias="_id")
-    type: str
     parameters: ModelParameters
+    max_model_tokens: int
+
+
+class SummarizerConfig(ModelConfig):
+    system_message: str
+    user_message_prefix: str
+
+
+class CardGenerationConfig(ModelConfig):
+    type: str
     examples: List[Example]
     card_prefix: str
     note_prefix: str
