ca81c96e5a2a4feca6b4dfd020084ea51cecf39e
Moritz Eich <hey@moritz.dev>
2023-08-26 22:44:41 +0200

    improve: exception handling

--- util/error.py
+++ util/error.py
@@ -12,7 +12,7 @@ def retry_on_exception(exception, max_retries=3, sleep_time=5):
                     retries += 1
                     if retries == max_retries:
                         raise Exception(
-                            f"Failed to execute {func.__name__} after {max_retries} retries."
+                            f"Failed to execute {func.__name__} after {max_retries} retries due to error: {e}"
                         ) from e
                 time.sleep(sleep_time)
 
