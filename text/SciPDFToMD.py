c69ffb6b044175064d05ed1a08d3d3199422bfb4
Moritz Eich <hey@moritz.dev>
2023-12-10 20:34:34 +0100

    improve: md postprocessing

--- text/SciPDFToMD.py
+++ text/SciPDFToMD.py
@@ -20,6 +20,114 @@ class SciPDFToMDMock(SciPDFToMDInterface):
         return "This is a mock"
 
 
+def find_next_punctuation(s: str, start_inx=0):
+    """
+    Find the index of the next punctuation mark
+
+    Args:
+        s: String to examine
+        start_inx: Index where to start
+    """
+
+    for i in range(start_inx, len(s)):
+        if s[i] in [".", "?", "!", "\n"]:
+            return i
+
+    return None
+
+
+def find_last_punctuation(s: str, start_inx=0):
+    """
+    Find the index of the last punctuation mark before start_inx
+
+    Args:
+        s: String to examine
+        start_inx: Index where to look before
+    """
+
+    for i in range(start_inx - 1, 0, -1):
+        if s[i] in [".", "?", "!", "\n"]:
+            return i
+
+    return None
+
+
+def truncate_repetitions(s: str, min_len=30):
+    """
+    Attempt to truncate repeating segments in the input string.
+
+    This function looks for the longest repeating substring at the end of the input string and truncates
+    it to appear only once. To be considered for removal, repetitions need to be continuous.
+
+    Args:
+        s (str): The input raw prediction to be truncated.
+        min_len (int): The minimum length of the repeating segment.
+
+    Returns:
+        str: The input string with repeated segments truncated.
+    """
+    s_lower = s.lower()
+    s_len = len(s_lower)
+
+    if s_len < 2 * min_len:
+        return s
+
+    # try to find a length at which the tail is repeating
+    max_rep_len = None
+    for rep_len in range(min_len, int(s_len / 2)):
+        # check if there is a repetition at the end
+        same = True
+        for i in range(0, rep_len):
+            if s_lower[s_len - rep_len - i - 1] != s_lower[s_len - i - 1]:
+                same = False
+                break
+
+        if same:
+            max_rep_len = rep_len
+
+    if max_rep_len is None:
+        return s
+
+    lcs = s_lower[-max_rep_len:]
+
+    # remove all but the last repetition
+    st = s
+    st_lower = s_lower
+    while st_lower.endswith(lcs):
+        st = st[:-max_rep_len]
+        st_lower = st_lower[:-max_rep_len]
+
+    # this is the tail with the repetitions
+    repeating_tail = s_lower[len(st_lower) :]
+
+    # add until next punctuation and make sure last sentence is not repeating
+    st_lower_out = st_lower
+    while True:
+        sentence_end = find_next_punctuation(s_lower, len(st_lower_out))
+        sentence_start = find_last_punctuation(s_lower, len(st_lower_out))
+        if sentence_end and sentence_start:
+            sentence = s_lower[sentence_start:sentence_end]
+            st_lower_out = s_lower[: sentence_end + 1]
+            if sentence in repeating_tail:
+                break
+        else:
+            break
+
+    s_out = s[: len(st_lower_out)]
+
+    return s_out
+
+
+def close_envs(s: str) -> str:
+    """checks if table envs are opened but not closed. Appends the closing statements and returns the new string"""
+    envs = ("bmatrix", "pmatrix", "matrix", "tabular", "table")
+    for env in envs:
+        begins, ends = s.count(r"\begin{%s}" % env), s.count(r"\end{%s}" % env)
+        if begins > ends:
+            s += (r"\end{%s}" % env) * (begins - ends)
+    return s
+
+
 class SciPDFToMD(SciPDFToMDInterface):
     _dpi: int = 96
     _modal_app_name: str = "nougat-ocr"
@@ -67,7 +175,7 @@ class SciPDFToMD(SciPDFToMDInterface):
 
     def _temp_replace_latex_in_table(self, text: str) -> tuple[str, dict]:
         latex_mapping = {}
-        latex_math_pattern = r"(\$\$?.*?\$\$?)"
+        latex_math_pattern = r"(\$\$?.*?\$\$?)|(\*\*.*?\*\*)"
 
         def replace_latex(match):
             table_content = match.group(0)
@@ -112,6 +220,8 @@ class SciPDFToMD(SciPDFToMDInterface):
 
     def _postprocess(self, markdown: list[str]) -> str:
         markdown_post_processed = "\n\n".join(markdown)
+
+        markdown_post_processed = truncate_repetitions(markdown_post_processed, 10)
 
         markdown_post_processed = self._replace_delimiters(markdown_post_processed)
 
